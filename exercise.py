732c3615469044acd49970d30b65ac5633355e75
Brued <Bruedbes@gmail.com>
2024-06-30 16:49:24 -0300

    adicionar ex5

--- exercise.py
+++ exercise.py
@@ -1,3 +1,17 @@
+# 1 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+# x = int(input("Digite um numero inteiro: "))
+# y = int(input("Digite um segundo numero inteiro: "))
+#sum = x + y
+
+#print("A Soma dos dois é: ", sum)
+
+# 2 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# x = int(input("Digite um numero inteiro: "))
+# y = int(input("Digite um segundo numero inteiro: "))
+# x = int (input("Digite um numero: "))
+# resul = x % 5
+# print ("O resto da divisão por 5 é: ", resul)
 
 # 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 #x = int(input("Digite um numero: "))
@@ -8,7 +22,13 @@
 
 # 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
-x = int (input("Digite o primeiro numero: "))
-y = int (input("Digite o segundo numero: "))
-div = x // y 
-print("O resultado da divisão inteira é: ", div)
+# x = int (input("Digite o primeiro numero: "))
+# y = int (input("Digite o segundo numero: "))
+# div = x // y 
+# print("O resultado da divisão inteira é: ", div)
+
+# 5 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+x = int(input("Digite um numero: "))
+resul = x **2
+print("O quadrado dele é: ", resul)
+
