db8bfb9025e4a0c5d8849ad86129d5d5061b8c85
Brued <Bruedbes@gmail.com>
2024-06-30 16:44:29 -0300

    adicionar ex4

--- exercise.py
+++ exercise.py
@@ -1,13 +1,14 @@
 
-
-
-
-
-
-
 # 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-x = int(input("Digite um numero: "))
-y = int(input("Digite o segundo numero: "))
+#x = int(input("Digite um numero: "))
+#y = int(input("Digite o segundo numero: "))
 
-mult = x * y 
-print("A multiplicação deles é: ", mult)
+#mult = x * y 
+#print("A multiplicação deles é: ", mult)
+
+# 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+x = int (input("Digite o primeiro numero: "))
+y = int (input("Digite o segundo numero: "))
+div = x // y 
+print("O resultado da divisão inteira é: ", div)
