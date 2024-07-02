209a2e49b83a8d94849e77cb7533168ffb22fe7b
Brued <Bruedbes@gmail.com>
2024-07-01 22:29:48 -0300

    add ex15

--- ex.py
+++ ex.py
@@ -92,8 +92,14 @@ import math
 
 # 14 - Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, 
 # imprima o dia, o mês e o ano separadamente.
-dma= input("Digite a data/ mes / ano: ")
-dia, mes, ano = dma.split("/") 
-print("Dia: ", dia)
-print("Mes: ", mes)
-print("Ano: ", ano)
+# dma= input("Digite a data/ mes / ano: ")
+# dia, mes, ano = dma.split("/") 
+# print("Dia: ", dia)
+# print("Mes: ", mes)
+# print("Ano: ", ano)
+
+# 15 - Escreva um programa que concatene duas strings fornecidas pelo usuário.
+ # string1 = input("Insira a parte um: ")
+# string2 = input(" Insira a parte dois: ")
+# resultado = string1 + " " + string2
+# print("Concatenando: ", resultado)
